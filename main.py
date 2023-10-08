cc33de2efa44bc4166b316c1413acc355dd0ce3a
Evgeniy Ivlev <jenia.ivlev@gmail.com>
2023-10-08 00:22:10 -0400

    Avoid eating all the CPU

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ app.include_router(server._router)
 
 def start():
     """Launched with `poetry run start` at root level"""
-    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
 
 
-if __name__ ==  "__main__":
+if __name__ == "__main__":
     start()
